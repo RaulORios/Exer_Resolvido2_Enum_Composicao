3a242936267b105fa1fc40b9dc9fdaef5afbb92e
RaulORios <raul.orios89@outlook.com>
2021-10-05 21:26:25 -0300

    Arquivos adicionados

---
+++ src/Exer_Resolvido2_Enum_Composicao/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exer_Resolvido2_Enum_Composicao.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment ()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+
+}
